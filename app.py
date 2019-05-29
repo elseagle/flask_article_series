4fb22dfd3769be43ea83e8faafef8d8a2f61bc8d
Oluwasogo Ogundowole <dowolebolu@gmail.com>
2019-05-29 14:43:23 +0100

    part-1

--- app.py
+++ app.py
@@ -1,99 +1,45 @@
-
-import sqlite3
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+store =  
+    {
+        "item": [
+            {
+                "name": 'Book',
+                "price": 99.99
+            }
+        ]
+    }
 
-
-def find_by_name(name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return jsonify({'item': {'name': row[0], 'price': row[1]}})
-
-
-@app.route('/item/<string:name>')
-def get(name):
-        item = find_by_name(name)
-        if item:
-            return item
-        return jsonify({'message': 'Item not found'}), 404
-
-
+	
 @app.route('/item')
-def get():
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return jsonify({'items': items})
-
-
-def insert(item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO {table} VALUES(?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
-
-        
+def get_item():
+    
+    return jsonify({"items": store})
 
 @app.route('/item', methods=['POST'])
-def post(name):
-        if find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}
+def create_item():
+    r = request.get_json()
+    new_item = {
+               "name": r['name'],
+               "price":r['price']
+           }
+    store['item'].append(new_item)
 
-        data = request.get_json()
+    return jsonify(new_item)
 
-        item = {'name': name, 'price': data['price']}
-
-        try:
-            insert(item)
-        except:
-            return jsonify({"message": "An error occurred inserting the item."})
-
-        return jsonify(item)
+@app.route('/item/<string:name>')
+def get_item(name):
+    for item in store['items']:
+        if name == item['name']:
+            return jsonify({"item":item})
 
 @app.route('/item/<string:name>')
 def delete_item(name):
-  connection = sqlite3.connect('data.db')
-  cursor = connection.cursor()
+    for item in store['items']:
+        if name == item['name']:
+            store['items'].remove(item)
+            return jsonify({"message":"item['name'] has been deleted"})
 
-  query = "DELETE FROM items WHERE name=?"
-  cursor.execute(query, (name,))
-
-  connection.commit()
-  connection.close()
-
-  return jsonify({'message': 'Item deleted'})
-
-
-def update(item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-
-        connection.commit()
-        connection.close()
-
-if __name__=="__main__":
-    app.run(port=4098)
+app.run(port=4098)
